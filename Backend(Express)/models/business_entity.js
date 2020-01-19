51251e7b1f8f3e424c76658fb53902f917af0f84
Edson Andrez <edson.andrez@outlook.es>
2020-01-19 01:45:40 -0600

    Relationship. Business Entity has one person.

--- Backend(Express)/models/business_entity.js
+++ Backend(Express)/models/business_entity.js
@@ -1,10 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const business_entity = sequelize.define('business_entity', {
-    business_entity_id: DataTypes.INTEGER
-  }, {});
-  business_entity.associate = function(models) {
-    // associations can be defined here
-  };
-  return business_entity;
+    const business_entity = sequelize.define('business_entity', {
+        business_entity_id: DataTypes.INTEGER
+    }, {});
+    business_entity.associate = function (models) {
+        // associations can be defined here
+        business_entity.hasOne(models.person, {through: 'business_entity_id', as: 'person'})
+    };
+    return business_entity;
 };

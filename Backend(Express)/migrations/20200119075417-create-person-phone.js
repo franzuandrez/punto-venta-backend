718b5e211729d4dcfbb020f9b36735a7f4d3b103
Edson Andrez <edson.andrez@outlook.es>
2020-01-19 02:07:06 -0600

    Person Phone model and migration.

---
+++ Backend(Express)/migrations/20200119075417-create-person-phone.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('person_phone', {
+      business_entity_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model:'business_entity',
+          key:'business_entity_id'
+        }
+      },
+      phone_number: {
+        type: Sequelize.STRING
+      },
+      phone_number_type_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model:'phone_number_type',
+          key:'phone_number_type_id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('person_phone');
+  }
+};

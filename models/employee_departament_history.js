db57c2167ea990441e004253b5de0347eb3f016d
Edson Andrez <edson.andre@outlook.es>
2020-05-18 14:48:50 -0600

    departament-history migration and model.

---
+++ models/employee_departament_history.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const employee_departament_history = sequelize.define('employee_departament_history', {
+    business_entity_id: DataTypes.INTEGER,
+    departament_id: DataTypes.INTEGER,
+    start_date: DataTypes.DATE,
+    end_date: DataTypes.DATE,
+    shift_id: DataTypes.INTEGER
+  }, {});
+  employee_departament_history.associate = function(models) {
+    // associations can be defined here
+  };
+  return employee_departament_history;
+};

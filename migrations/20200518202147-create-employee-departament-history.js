33a1657ec064787ab02b5e05c5d758350b9f8c69
Jairo Velásquez <jairo.j5180@gmail.com>
2020-06-28 16:04:48 -0600

    Creating relationship between shift and employee_departament_history

--- migrations/20200518202147-create-employee-departament-history.js
+++ migrations/20200518202147-create-employee-departament-history.js
@@ -1,39 +1,43 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('employee_departament_histories', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      business_entity_id: {
-        type: Sequelize.INTEGER
-      },
-      departament_id: {
-        type: Sequelize.INTEGER
-      },
-      start_date: {
-        type: Sequelize.DATE
-      },
-      end_date: {
-        type: Sequelize.DATE
-      },
-      shift_id: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('employee_departament_histories');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('employee_departament_histories', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            business_entity_id: {
+                type: Sequelize.INTEGER
+            },
+            departament_id: {
+                type: Sequelize.INTEGER
+            },
+            start_date: {
+                type: Sequelize.DATE
+            },
+            end_date: {
+                type: Sequelize.DATE
+            },
+            shift_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'shift',
+                    key: 'shift_id'
+                }
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('employee_departament_histories');
+    }
 };

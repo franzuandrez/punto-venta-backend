de7a75a24dacec43755ed1dc6b5590e09a5e4316
Jairo Velásquez <jairo.j5180@gmail.com>
2020-06-28 18:18:13 -0600

    Creating relationship between product_list_price_hisrory.js and product.js

--- migrations/20200119070144-create-product-list-price-history.js
+++ migrations/20200119070144-create-product-list-price-history.js
@@ -1,33 +1,40 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('product_list_price_history', {
-      product_id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      start_date: {
-        type: Sequelize.DATE
-      },
-      end_date: {
-        type: Sequelize.DATE
-      },
-      list_price: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('product_list_price_history');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('product_list_price_history', {
+            product_list_price_id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            product_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'product',
+                    key: 'product_id'
+                }
+            },
+            start_date: {
+                type: Sequelize.DATE
+            },
+            end_date: {
+                type: Sequelize.DATE
+            },
+            list_price: {
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('product_list_price_history');
+    }
 };

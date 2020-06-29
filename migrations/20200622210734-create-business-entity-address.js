1261d6e585970379e25ccfa3a51b79c1bb02f7fc
Jairo Velásquez <jairo.j5180@gmail.com>
2020-06-28 20:17:37 -0600

    Creating relationship between business_entity.js and business_entity_address

--- migrations/20200622210734-create-business-entity-address.js
+++ migrations/20200622210734-create-business-entity-address.js
@@ -1,31 +1,37 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('business_entity_address', {
-
-      business_entity_id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      address_id: {
-        type: Sequelize.INTEGER
-      },
-      address_type_id: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('business_entity_address');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('business_entity_address', {
+            busines_address_id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            business_entity_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'business_entity',
+                    key: 'business_entity_id'
+                }
+            },
+            address_id: {
+                type: Sequelize.INTEGER
+            },
+            address_type_id: {
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('business_entity_address');
+    }
 };
